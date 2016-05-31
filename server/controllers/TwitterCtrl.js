4e3af080b8bedbd4f39a271f3c492984d918cc31
Donovan Hiland <donovan.hiland@gmail.com>
2016-05-31 14:15:48 -0600

    es6 linting. twitter api tests

--- server/controllers/TwitterCtrl.js
+++ server/controllers/TwitterCtrl.js
@@ -14,7 +14,7 @@ module.exports = {
         //=== gets followers ids from screen name ===//
         twitter.get('followers/ids', {
             screen_name: screenName,
-            count: 5000
+            count: 20
                 // this call can get up to 5000 ids, and can be called 15 times every 15 minutes. 75,000 followers can be retrieved every 15 minutes.
         }, (error, ids, response) => {
             // error is the error message
@@ -30,11 +30,17 @@ module.exports = {
                     // this call can be passed a maximum of 100 user ids at a time. With user auth the endpoint can be hit 180 times every 15 minutes: 18,000 users every 15 minutes. With app auth the endpoint can be hit 60 times every 15 minutes: 6000 users every 15 minutes.
             }, (error, user, response) => {
                 if (error) console.log(error);
-                for (let i = 0; i < user.length; i++) {
-                    console.log(user[i].location);
+                let trimmedArray = [];
+                for(let i = 0; i < user.length; i++) {
+                  if(user[i].location) {
+                    trimmedArray.push(user[i].location);
+                  }
                 }
-            })
+                for (let i = 0; i < trimmedArray.length; i++) {
+                    console.log(trimmedArray[i]);
+                }
+            });
         });
     }
 
-}
+};
