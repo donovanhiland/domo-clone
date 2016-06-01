949f540475c9348a5d05471ba07acf78084a0f65
Spencer Kekauoha <spkekauoha@gmail.com>
2016-05-31 22:29:31 -0600

    domo solution cards need adjustments for mobile

--- public/assets/js/jquery-slider.js
+++ public/assets/js/jquery-slider.js
@@ -1,9 +1,18 @@
+// window.onload=function(){
+//   $('.carousel').slick({
+//     autoplay: true,
+//     infinite: true,
+//     autoplaySpeed: 2000,
+//     slidesToShow: 2,
+//     slidesToScroll: 1
+//   });
+// };
+
+
+
+
 window.onload=function(){
+  console.log('ready');
   $('.carousel').slick({
-    autoplay: true,
-    infinite: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 2,
-    slidesToScroll: 1
   });
 };
