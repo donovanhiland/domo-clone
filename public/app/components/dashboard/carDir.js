65cd90caccb2839743173b51883c76e783da6fa3
Spencer Kekauoha <spkekauoha@gmail.com>
2016-06-02 18:55:19 -0600

    bug fixes

--- public/app/components/dashboard/carDir.js
+++ public/app/components/dashboard/carDir.js
@@ -4,9 +4,15 @@ angular.module('domoApp').directive('cardDirective', function(){
       restrict: 'A',
       link: function (scope, element, attrs) {
 
-        $('.card-btn').on('click', function(){
+        $('.card-lg').on('click', function(){
           $(this).parent().css('height', '80vh');
-          $(this).parent().css('width', '80vh');
+          $(this).parent().css('width', '80vw');
+          // $(this).parent().css('transition', 'all 0.9s ease-in-out');
+        });
+
+        $('.card-sm').on('click', function(){
+          $(this).parent().css('height', '40vh');
+          $(this).parent().css('width', '20vw');
         })
 
       }
