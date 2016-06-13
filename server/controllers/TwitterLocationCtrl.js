7909a588dac4190c23971acbbc12aaa9d6ef2f00
Donovan Hiland <donovan.hiland@gmail.com>
2016-06-13 09:10:28 -0600

    instagram tests, globe tweaks

--- server/controllers/TwitterLocationCtrl.js
+++ server/controllers/TwitterLocationCtrl.js
@@ -69,193 +69,211 @@ const twitter = new Twitter({
 //     });
 // };
 
+let idsList,
+idsListFiltered,
+locationName,
+latitude,
+longitude,
+locationData = {},
+index = 0,
+locationList = [],
+cursorTracker,
+ipRegExp = new RegExp('^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$');
+//call for followers ids
+const getFollowersIdsAsync = (screenName, cursor = -1) => {
+  return new Promise((resolve, reject) => {
+    twitter.get('followers/ids', {
+      screen_name: screenName,
+      count: 2400,
+      cursor: cursor
+    }, (error, ids, response) => {
+      if (error) console.log(error);
+      //ids.ids is the array of follower ids
+      idsList = ids.ids;
+      idsListFiltered = idsList.filter((id) => {
+        if (id.toString().length < 11) return id;
+      });
+      cursorTracker = ids.next_cursor;
+      resolve(idsListFiltered);
+    });
+  });
+};
+
+const getUsersAsync = (idsList) => {
+  return new Promise((resolve, reject) => {
+    const getUsersById = (idsList) => {
+      // users/lookup only takes 100 ids at a time as a string of ids separated by commas
+      let idsListJoined = idsList.slice(index, index + 100).join(',');
+      if (idsListJoined) {
+        // calls for user objects based on a batch of user ids
+        twitter.get('users/lookup', {
+          user_id: idsListJoined
+        }, (error, users, response) => {
+          _.forEach(users, (user) => {
+            // push the location on the user object to locationList
+            if (user.location && !ipRegExp.test(user.location)) locationList.push(user.location);
+          });
+          index += 100;
+          getUsersById(idsList);
+        });
+      }
+      // if the loop gets to the end of the ids list then idsListJoined will be an empty string "" - falsey.
+      if (!idsListJoined) {
+        resolve(locationList);
+      }
+    };
+    // loop through function again until all the ids have been searched
+    getUsersById(idsList);
+  });
+};
+
+const getLocationByQueryAsync = (locationList) => {
+  return new Promise((resolve, reject) => {
+    index = 0;
+    const getLocationByQuery = (locationList) => {
+      if (locationList[index]) {
+        geocoder.geocode(locationList[index], function(error, res) {
+          // res is a list of location objects returned based on the queries location
+          if (error) {
+            console.log(error);
+            console.log(locationList[index]);
+            index++;
+            getLocationByQuery(locationList);
+          }
+          // if response is empty, do nothing. if there is a response, get the level1/2/3long or country property and set locationName equal to that value
+          // if(res === undefined)
+          if (!res[0]) {
+            index++;
+            getLocationByQuery(locationList);
+          }
+          if (res[0]) {
+            // if there's a response, but there's no level1long property
+            if (res[0].administrativeLevels.level1long) {
+              locationName = res[0].administrativeLevels.level1long;
+            }
+            if (!res[0].administrativeLevels.level1long) {
+              if (res[0].administrativeLevels.level2long) {
+                locationName = res[0].administrativeLevels.level2long;
+              }
+              if (!res[0].administrativeLevels.level2long) {
+                if (res[0].administrativeLevels.level3long) {
+                  locationName = res[0].administrativeLevels.level3long;
+                }
+                if (!res[0].administrativeLevels.level3long) {
+                  locationName = res[0].country;
+                }
+              }
+            }
+            longitude = res[0].longitude;
+            latitude = res[0].latitude;
+          }
+          // if locationData has a property of locationName (which is the state returned from the search), add 1 to the tally
+          if (locationData.hasOwnProperty(locationName)) locationData[locationName].count += 1;
+          // if locationData doesn't have property represented by locationName, create it and start the tally
+          if (!locationData.hasOwnProperty(locationName)) {
+            locationData[locationName] = {
+              count: 1,
+              latitude: latitude,
+              longitude: longitude
+            };
+          }
+          index++;
+          getLocationByQuery(locationList);
+        });
+      }
+      if (!locationList[index] || index > 2399) {
+        locationData.cursorTracker = cursorTracker;
+        locationData.date = new Date();
+        resolve(locationData);
+      }
+    };
+    getLocationByQuery(locationList);
+  });
+};
+
+const createLocationDocumentAsync = (locationData) => {
+  return new Promise((resolve, reject) => {
+    let twitterLocations = {
+      data: []
+    };
+    twitterLocations.date = locationData.date;
+    twitterLocations.cursor = locationData.cursorTracker;
+    for (let prop in locationData) {
+      if (locationData.hasOwnProperty(prop) && prop !== 'date' && prop !== 'cursorTracker') {
+        twitterLocations.data.push(locationData[prop]);
+      }
+    }
+    TwitterLocation.create(twitterLocations, (error, response) => {
+      if (error) console.log(error);
+      resolve(response);
+    });
+  });
+};
+
+const formatLocationDataAsync = (twitterLocations) => {
+  return new Promise((resolve, reject) => {
+    // TwitterLocation.find({})
+    let latitude,
+    longitude,
+    magnitude,
+    color,
+    max,
+    colorRange = (1 / 11),
+    locationJSON = [];
+    // webGL globe takes a max magnitude of 8, but to be visible a max magnitude of 4.
+    // I need to scale the tally field of followers to between 0 and 4
+    // Divide 4 by the max tally to get the magic number I need to scale everything down.
+    for (let i = 0; i < twitterLocations.data.length; i++) {
+      // find the highest tally so I can use it to scale the others based off of it
+      if (max === undefined || max < twitterLocations.data[i].count) max = twitterLocations.data[i].count;
+    }
+    let scaler = (1 / max);
+    for (let i = 0; i < twitterLocations.data.length; i++) {
+      // accepted format = [long, lat, mag, color(0 through list of colors defined)]
+      if (twitterLocations.data[i] !== 'date' && twitterLocations.data[i] !== 'cursorTracker') {
+        longitude = Number(twitterLocations.data[i].longitude.toFixed(3));
+        latitude = Number(twitterLocations.data[i].latitude.toFixed(3));
+        magnitude = Number((twitterLocations.data[i].count * scaler).toFixed(3));
+        if (magnitude >= colorRange * 0 && magnitude <= colorRange * 1) color = 0;
+        if (magnitude >= colorRange * 1 && magnitude <= colorRange * 2) color = 1;
+        if (magnitude >= colorRange * 2 && magnitude <= colorRange * 3) color = 2;
+        if (magnitude >= colorRange * 3 && magnitude <= colorRange * 4) color = 3;
+        if (magnitude >= colorRange * 4 && magnitude <= colorRange * 5) color = 4;
+        if (magnitude >= colorRange * 5 && magnitude <= colorRange * 6) color = 5;
+        if (magnitude >= colorRange * 6 && magnitude <= colorRange * 7) color = 6;
+        if (magnitude >= colorRange * 7 && magnitude <= colorRange * 8) color = 7;
+        if (magnitude >= colorRange * 8 && magnitude <= colorRange * 9) color = 8;
+        if (magnitude >= colorRange * 9 && magnitude <= colorRange * 10) color = 9;
+        if (magnitude >= colorRange * 10 && magnitude <= colorRange * 11) color = 10;
+        locationJSON.push(latitude, longitude, magnitude, color);
+      }
+    }
+    resolve(locationJSON);
+  });
+};
+
+const handleErr = (error) => {
+  console.log('there was an error', error);
+};
+
 module.exports = {
     getDataByScreenName: (req, res, next) => {
-
-        let idsList,
-            locationName,
-            latitude,
-            longitude,
-            locationData = {},
-            index = 0,
-            locationList = [],
-            cursorTracker;
-        //call for followers ids
-        const getFollowersIdsAsync = (screenName, cursor = -1) => {
-            return new Promise((resolve, reject) => {
-                twitter.get('followers/ids', {
-                    screen_name: screenName,
-                    count: 3,
-                    cursor: cursor
-                }, (error, ids, response) => {
-                    if (error) console.log(error);
-                    //ids.ids is the array of follower ids
-                    idsList = ids.ids;
-                    cursorTracker = ids.next_cursor;
-                    resolve(idsList);
-                });
-            });
-        };
-
-        const getUsersAsync = (idsList) => {
-            return new Promise((resolve, reject) => {
-                const getUsersById = (idsList) => {
-                    // users/lookup only takes 100 ids at a time as a string of ids separated by commas
-                    let idsListJoined = idsList.slice(index, index + 100).join(',');
-                    if (idsListJoined) {
-                        // calls for user objects based on a batch of user ids
-                        twitter.get('users/lookup', {
-                            user_id: idsListJoined
-                        }, (error, users, response) => {
-                            _.forEach(users, (user) => {
-                                // push the location on the user object to locationList
-                                if (user.location) locationList.push(user.location);
-                            });
-                            index += 100;
-                            getUsersById(idsList);
-                        });
-                    }
-                    // if the loop gets to the end of the ids list then idsListJoined will be an empty string "" - falsey.
-                    if (!idsListJoined) {
-                        resolve(locationList);
-                    }
-                };
-                // loop through function again until all the ids have been searched
-                getUsersById(idsList);
-            });
-        };
-
-        const getLocationByQueryAsync = (locationList) => {
-            return new Promise((resolve, reject) => {
-                index = 0;
-                const getLocationByQuery = (locationList) => {
-                    if (locationList[index]) {
-                        geocoder.geocode(locationList[index], function(error, res) {
-                            // res is a list of location objects returned based on the queries location
-                            if (error) console.log(error);
-                            // if response is empty, do nothing. if there is a response, get the level1/2/3long or country property and set locationName equal to that value
-                            if (!res[0]) {
-                                index++;
-                                getLocationByQuery(locationList);
-                            }
-                            if (res[0]) {
-                                // if there's a response, but there's no level1long property
-                                if (res[0].administrativeLevels.level1long) {
-                                    locationName = res[0].administrativeLevels.level1long;
-                                }
-                                if (!res[0].administrativeLevels.level1long) {
-                                    if (res[0].administrativeLevels.level2long) {
-                                        locationName = res[0].administrativeLevels.level2long;
-                                    }
-                                    if (!res[0].administrativeLevels.level2long) {
-                                        if (res[0].administrativeLevels.level3long) {
-                                            locationName = res[0].administrativeLevels.level3long;
-                                        }
-                                        if (!res[0].administrativeLevels.level3long) {
-                                            locationName = res[0].country;
-                                        }
-                                    }
-                                }
-                                longitude = res[0].longitude;
-                                latitude = res[0].latitude;
-                            }
-                            // if locationData has a property of locationName (which is the state returned from the search), add 1 to the tally
-                            if (locationData.hasOwnProperty(locationName)) locationData[locationName].count += 1;
-                            // if locationData doesn't have property represented by locationName, create it and start the tally
-                            if (!locationData.hasOwnProperty(locationName)) {
-                                locationData[locationName] = {
-                                    count: 1,
-                                    latitude: latitude,
-                                    longitude: longitude
-                                };
-                            }
-                            index++;
-                            getLocationByQuery(locationList);
-                        });
-                    }
-                    if (!locationList[index]) {
-                        locationData.cursorTracker = cursorTracker;
-                        locationData.date = new Date();
-                        resolve(locationData);
-                    }
-                };
-                getLocationByQuery(locationList);
-            });
-        };
-
-        const createLocationDocumentAsync = (locationData) => {
-            return new Promise((resolve, reject) => {
-                let twitterLocations = {
-                    data: []
-                };
-                twitterLocations.date = locationData.date;
-                twitterLocations.cursor = locationData.cursorTracker;
-                for (let prop in locationData) {
-                    if (locationData.hasOwnProperty(prop) && prop !== 'date' && prop !== 'cursorTracker') {
-                        twitterLocations.data.push(response[prop]);
-                    }
-                }
-                TwitterLocation.create(twitterLocations, (error, response) => {
-                    if (error) console.log(error);
-                    resolve(response);
-                });
-            });
-        };
-
-        const formatLocationDataAsync = (twitterLocations) => {
-            return new Promise((resolve, reject) => {
-                // TwitterLocation.find({})
-                let latitude,
-                    longitude,
-                    magnitude,
-                    color,
-                    max,
-                    colorRange = (1 / 11),
-                    locationJSON = [];
-                // webGL globe takes a max magnitude of 8, but to be visible a max magnitude of 4.
-                // I need to scale the tally field of followers to between 0 and 4
-                // Divide 4 by the max tally to get the magic number I need to scale everything down.
-                for (let i = 0; i < twitterLocations.data.length; i++) {
-                    // find the highest tally so I can use it to scale the others based off of it
-                    if (max === undefined || max < twitterLocations.data[i].count) max = twitterLocations.data[i].count;
-                }
-                let scaler = (0.75 / max);
-                for (let i = 0; i < twitterLocations.data.length; i++) {
-                    // accepted format = [long, lat, mag, color(0 through list of colors defined)]
-                    if(twitterLocations.data[i] !== 'date' && twitterLocations.data[i] !== 'cursorTracker') {
-                      longitude = Number(twitterLocations.data[i].longitude.toFixed(3));
-                      latitude = Number(twitterLocations.data[i].latitude.toFixed(3));
-                      magnitude = Number((twitterLocations.data[i].count * scaler).toFixed(3));
-                      if (magnitude >= colorRange * 0 && magnitude <= colorRange * 1) color = 0;
-                      if (magnitude >= colorRange * 1 && magnitude <= colorRange * 2) color = 1;
-                      if (magnitude >= colorRange * 2 && magnitude <= colorRange * 3) color = 2;
-                      if (magnitude >= colorRange * 3 && magnitude <= colorRange * 4) color = 3;
-                      if (magnitude >= colorRange * 4 && magnitude <= colorRange * 5) color = 4;
-                      if (magnitude >= colorRange * 5 && magnitude <= colorRange * 6) color = 5;
-                      if (magnitude >= colorRange * 6 && magnitude <= colorRange * 7) color = 6;
-                      if (magnitude >= colorRange * 7 && magnitude <= colorRange * 8) color = 7;
-                      if (magnitude >= colorRange * 8 && magnitude <= colorRange * 9) color = 8;
-                      if (magnitude >= colorRange * 9 && magnitude <= colorRange * 10) color = 9;
-                      if (magnitude >= colorRange * 10 && magnitude <= colorRange * 11) color = 10;
-                      locationJSON.push(latitude, longitude, magnitude, color);
-                    }
-                }
-                resolve(locationJSON);
-            });
-        };
-
-        const handleErr = (error) => {
-            console.log('there was an error', error);
-        };
-
         getFollowersIdsAsync(req.body.screenName)
             .then(getUsersAsync, handleErr)
             .then(getLocationByQueryAsync, handleErr)
             .then(createLocationDocumentAsync, handleErr)
-            .then(console.log, handleErr);
-            // .then(formatLocationDataAsync, handleErr);
-        // .then((response) => {
-        //   res.status(200).json(response);
-        // });
+            .then(formatLocationDataAsync, handleErr)
+            .then((response) => {
+                res.status(200).json(response);
+            });
+    },
+
+    formatLocationDataFromDB: (req, res, next) => {
+      TwitterLocation.findOne({}, (error, response) => {
+        formatLocationDataAsync(response)
+        .then((response) => {
+          res.status(200).send(response);
+        });
+      });
     }
 };
